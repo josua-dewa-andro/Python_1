fcaf8fd1e1a95ad7a91524ae22ebcc3d8d082100
jojoE2412 <joshuaemma2412@gmail.com>
2025-08-30 00:13:40 +0700

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,24 @@
+var= '1'
+
+var2=[]
+
+print(var*100)
+
+print(1+1)
+print(1-2)
+print(1/2)
+print(1*2)
+print(1**2)
+print(1%2)
+
+a=6
+b=input('masukkan huruf: ')  
+
+print(a+len(b))
+
+if len(b) >= 5:
+    print(b*2) 
+elif len(b) == 4:
+    print("andro hama")
+else:
+    print(b*3)
