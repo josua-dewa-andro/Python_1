8ec37686dfade41cf06c67f16e59a69918988a06
Fuzyxd <rayhanfuzy@gmail.com>
2025-09-01 13:51:23 +0700

    Add files via upload

---
+++ T3.py
@@ -0,0 +1,24 @@
+var= '5'
+
+var2=[]
+
+print(var*1000)
+
+print(1+1)
+print(1-2)
+print(1/2)
+print(1*2)
+print(1**2)
+print(1%2)
+
+a=9
+b=input('huruf: ')  
+
+print(a+len(b))
+
+if len(b) >= 5:
+    print(b*2) 
+elif len(b) == 4:
+    print("andro hama")
+else:
+    print(b*3)
